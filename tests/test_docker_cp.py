4d63b8c7318fb8a3ecd3540b66599ff33ddeffa2
Juliano Fernandes <julianofernandes@gmail.com>
2019-06-25 18:58:58 -0300

    add test for validate method

--- tests/test_docker_cp.py
+++ tests/test_docker_cp.py
@@ -2,6 +2,7 @@ import sys
 from unittest import mock
 
 import pytest
+import schema
 
 from docker_cp import cli
 
@@ -20,3 +21,22 @@ def test_main_missing_required_argument(args):
     with mock.patch.object(sys, "argv", argv):
         with pytest.raises(SystemExit):
             cli.main()
+
+
+@pytest.mark.parametrize(
+    "arg,exp",
+    [
+        (
+            {"--buffer-length": "4.5"},
+            "Buffer length must be an integer greater than 0",
+        ),
+        ({"FILE": ""}, "Invalid input file"),
+        ({"TARGET": ""}, "Invalid destination directory"),
+    ],
+)
+def test_validate_raises_exception(arg, exp, args):
+    cmd = cli.CopyCommand(args)
+    args.update(arg)
+    with pytest.raises(schema.SchemaError) as error:
+        cmd.validate(args)
+    assert str(error.value) == exp

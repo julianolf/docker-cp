351ab7f38778c0b01aa2853606f12bc2041afef0
Juliano Fernandes <julianofernandes@gmail.com>
2019-06-25 20:16:30 -0300

    fix main function tests adding missing obj patch

--- tests/test_docker_cp.py
+++ tests/test_docker_cp.py
@@ -11,17 +11,19 @@ from docker_cp import cli
 def test_main(args):
     argv = ["docker_cp.cli", args["FILE"], args["TARGET"]]
     with mock.patch.object(sys, "argv", argv):
-        try:
-            cli.main()
-        except SystemExit:
-            assert not "Should not be raised"
+        with mock.patch("docker_cp.cli.docker"):
+            try:
+                cli.main()
+            except SystemExit:
+                assert not "Should not be raised"
 
 
 def test_main_missing_required_argument(args):
     argv = ["docker_cp.cli", args["FILE"]]
     with mock.patch.object(sys, "argv", argv):
-        with pytest.raises(SystemExit):
-            cli.main()
+        with mock.patch("docker_cp.cli.docker"):
+            with pytest.raises(SystemExit):
+                cli.main()
 
 
 @pytest.mark.parametrize(

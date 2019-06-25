9f93f3156faa1de4e0f13046573df703b669ba79
Juliano Fernandes <julianofernandes@gmail.com>
2019-06-25 17:27:26 -0300

    add initial cli
    - create main function
    - add input arguments parser
    - add usage and help description

---
+++ docker_cp/cli.py
@@ -0,0 +1,25 @@
+"""Copy file from or to a Docker container.
+
+Usage:
+    docker-cp [--buffer-length=<bytes>] FILE TARGET
+    docker-cp (-h | --help)
+    docker-cp --version
+
+Arguments:
+    FILE    Source file. e.g. mycontainer:/proc/version
+    TARGET  Destination directory. e.g. .
+
+Options:
+    --buffer-length=<bytes>  Buffer size in bytes.
+"""
+
+import docopt
+
+
+def main():
+    """Read input arguments."""
+    args = docopt.docopt(__doc__, version="0.1.0a1")
+
+
+if __name__ == "__main__":
+    main()
